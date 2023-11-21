ca97d6bba0eab2dc5e5142ba2c461f663c361462
Sophia <chiaki.katsuno@live.ca>
2023-11-20 19:23:44 -0800

    add props to PhotoListItem function

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,7 +1,8 @@
 // frontend/src/components/PhotoListItem.jsx
 import React from "react";
 
-const PhotoListItem = ({ data }) => {
+const PhotoListItem = (props) => {
+  const { data } = props;
   const { id, location, imageSource, username, profile } = data;
 
   return (
@@ -14,5 +15,19 @@ const PhotoListItem = ({ data }) => {
     </div>
   );
 };
+
+// ({ data }) => {
+//   const { id, location, imageSource, username, profile } = data;
+
+//   return (
+//     <div>
+//       <img src={imageSource} alt={`Photo by ${username}`} />
+//       <p>{`Photo ID: ${id}`}</p>
+//       <p>{`Location: ${location.city}, ${location.country}`}</p>
+//       <p>{`Photographer: ${username}`}</p>
+//       <img src={profile} alt={`Profile of ${username}`} />
+//     </div>
+//   );
+// };
 
 export default PhotoListItem;
