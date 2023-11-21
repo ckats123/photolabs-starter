468de73b08f374a923d7671d0663a0b07c977588
Sophia <chiaki.katsuno@live.ca>
2023-11-20 19:52:41 -0800

    add classNames for styling css

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -6,13 +6,24 @@ const PhotoListItem = (props) => {
   const { id, location, imageSource, username, profile } = data;
 
   return (
-    <div>
-      <img src={imageSource} alt={`Photo by ${username}`} />
-      <p>{`Photo ID: ${id}`}</p>
-      <p>{`Location: ${location.city}, ${location.country}`}</p>
-      <p>{`Photographer: ${username}`}</p>
-      <img src={profile} alt={`Profile of ${username}`} />
-    </div>
+    <article className="photo-list_item">
+      <img
+        src={imageSource}
+        alt={`Photo by ${username}`}
+        className="photo-list_image"
+      />
+      <div className="photo-list_user-details">
+        <img
+          src={imageSource}
+          alt={`Photo by ${username}`}
+          className="photo-list_user-profile"
+        />
+        <div className="photo-list__user-info">{`Photo ID: ${id}`}</div>
+        <div className="photo-list__user-location">{`Location: ${location.city}, ${location.country}`}</div>
+        <div className="photo-list__user-username">{`Photographer: ${username}`}</div>
+        <img src={profile} alt={`Profile of ${username}`} className="photo-list_user-profile"/>
+      </div>
+    </article>
   );
 };
 
