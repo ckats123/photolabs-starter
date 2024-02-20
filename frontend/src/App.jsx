6cbc8e82da3d7d6b42161e26317fd9a6d1fd50c4
Sophia <chiaki.katsuno@live.ca>
2024-02-19 18:59:18 -0800

    add topiclist and photolist to app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,19 @@
 // frontend/src/App.jsx
 import React from "react";
 import PhotoList from "./components/PhotoList";
+import TopicList from "components/TopicList";
 import TopNavigationBar from "./components/TopNavigationBar";
 import topics from "mocks/topics";
 import photos from "mocks/photos";
 
 import "./App.scss";
-import HomeRoute from "components/HomeRoute";
+// import HomeRoute from "components/HomeRoute";
 
 const App = () => (
   <div className="App">
-    <HomeRoute topics={topics} photos={photos} />
+    {/* <HomeRoute topics={topics} photos={photos} /> */}
+    <TopicList topics={topics} />
+    <PhotoList photos={photos} />
   </div>
 );
 
