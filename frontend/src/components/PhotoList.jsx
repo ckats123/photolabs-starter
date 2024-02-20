1a7dfd793be05765d24ebac5c531ed28656d1d06
Sophia <chiaki.katsuno@live.ca>
2024-02-19 19:58:25 -0800

    render more data

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -3,14 +3,16 @@ import PhotoListItem from "./PhotoListItem";
 import "../styles/PhotoList.scss";
 
 const PhotoList = (props) => {
-  const photos = props.photos.map((photoData) => (
-    <PhotoListItem
-      key={photoData.id}
-      data={photoData}
-      favoritedPhotos={props.favoritedPhotos}
-      setFavoritedPhotos={props.setFavoritedPhotos}
-    />
-  ));
+  const photos = props.photos.map((photoData) => {
+    return (
+      <PhotoListItem
+        key={photoData.id}
+        data={photoData}
+        favoritedPhotos={props.favoritedPhotos}
+        setFavoritedPhotos={props.setFavoritedPhotos}
+      />
+    );
+  });
   return <ul className="photo-list">{photos}</ul>;
 };
 
