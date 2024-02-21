a8de9c46e779c189f47ac547c73f52ac78201ae9
Sophia <chiaki.katsuno@live.ca>
2024-02-20 19:57:59 -0800

    create feature for closing modal

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -3,11 +3,16 @@ import React from 'react';
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
-const PhotoDetailsModal = () => {
+const PhotoDetailsModal = (props) => {
+  const { modalVisibility, setModalVisibility } = props;
+  if (!modalVisibility) {
+    return null;
+  }
+
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button">
-        <img src={closeSymbol} alt="close symbol" />
+        <img src={closeSymbol} alt="close symbol" onClick={() => setModalVisibility(false)} />
       </button>
     </div>
   )
