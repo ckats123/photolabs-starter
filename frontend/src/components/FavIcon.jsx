020a6bcc642c1d80c0034d3017cc755ddaf3728d
Sophia <chiaki.katsuno@live.ca>
2024-02-25 16:45:28 -0800

    adjust like button

--- frontend/src/components/FavIcon.jsx
+++ frontend/src/components/FavIcon.jsx
@@ -22,7 +22,7 @@ const FavIcon = ({ displayAlert, isFav }) => {
           cx="21"
           cy="4"
           r="2.75"
-          fill="#FFFF00"
+          fill="0ee84c"
           stroke="#C80000"
           strokeWidth="0.5"
         />
