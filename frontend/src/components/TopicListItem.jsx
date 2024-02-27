582dd200cf90717270f2d036d1800a06ad7ac790
Sophia <chiaki.katsuno@live.ca>
2024-02-26 20:59:40 -0800

    add click handle to onClosePhotoDetailsModal

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -2,15 +2,22 @@ import React from "react";
 
 import "../styles/TopicListItem.scss";
 
-const sampleDataForTopicListItem = {
-  id: "1",
-  slug: "topic-1",
-  label: "Nature",
-};
+// const sampleDataForTopicListItem = {
+//   id: "1",
+//   slug: "topic-1",
+//   label: "Nature",
+// };
 
 const TopicListItem = (props) => {
-  const { data } = props;
-  return <div className="topic-list__item"></div>;
+
+  const { data, onClosePhotoDetailsModal } = props;
+  const handleTopicClick = () => {
+    onClosePhotoDetailsModal(data);
+  };
+
+  return <div className="topic-list__item">
+    <span onClick={handleTopicClick}>{data.title}</span>
+  </div>;
 };
 
 export default TopicListItem;
