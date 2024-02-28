113952502c316e1843558184ba60cca50199133f
Sophia <chiaki.katsuno@live.ca>
2024-02-27 22:36:40 -0800

    some adjustments to styling

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,76 +1,33 @@
 // HomeRoute.jsx
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import TopNavigation from "components/TopNavigationBar";
 import PhotoList from "components/PhotoList";
 import "../styles/HomeRoute.scss";
-// import PhotoFavButton from "components/PhotoFavButton";
+
 
 const HomeRoute = (props) => {
   const {
     photos,
     topics,
+    onPhotoSelect,
     updateToFavPhotoIds,
-    // onPhotoClick,
     favoritedPhotos,
-    // setFavoritedPhotos,
-    // setActivePhoto,
-    setPhotoSelected,
     onLoadTopic,
   } = props;
-
-  // // logs favoritedPhotos
-  // useEffect(() => {
-  //   console.log("fav photos", favoritedPhotos);
-  // }, [favoritedPhotos]);
-
-  // // Retrieve favoritedPhotos from local storage on mount
-  // useEffect(() => {
-  //   const storedFavoritedPhotos =
-  //     localStorage.getItem("favoritedPhotos");
-
-  //   // Check if storedFavoritedPhotos is not null and is an array before parsing
-  //   if (storedFavoritedPhotos !== null) {
-  //     try {
-  //       const parsedFavoritedPhotos = JSON.parse(
-  //         storedFavoritedPhotos
-  //       );
-
-  //       if (Array.isArray(parsedFavoritedPhotos)) {
-  //         setFavoritedPhotos(parsedFavoritedPhotos);
-  //       } else {
-  //         console.error("Stored favorited photos is not an array.");
-  //       }
-  //     } catch (error) {
-  //       console.error(
-  //         "Error parsing stored favorited photos:",
-  //         error
-  //       );
-  //     }
-  //   }
-  // }, []);
-
-  // // Update local storage whenever favoritedPhotos changes
-  // useEffect(() => {
-  //   localStorage.setItem(
-  //     "favoritedPhotos",
-  //     JSON.stringify(favoritedPhotos)
-  //   );
-  // }, [favoritedPhotos]);
 
   return (
     <div className="home-route">
       <TopNavigation
         topics={topics}
-        isFavorited={favoritedPhotos.length > 0}
+        isFavPhotoExist={favoritedPhotos.length > 0}
         onLoadTopic={onLoadTopic}
       />
       <PhotoList
         photos={photos}
         favoritedPhotos={favoritedPhotos}
         updateToFavPhotoIds={updateToFavPhotoIds}
-        // onPhotoClick={onPhotoClick}
-        setPhotoSelected={setPhotoSelected}
+        onPhotoSelect={onPhotoSelect}
       />
     </div>
   );
--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -7,47 +7,49 @@ import "../styles/PhotoFavButton.scss";
 import closeSymbol from "../assets/closeSymbol.svg";
 import PhotoList from "components/PhotoList";
 import PhotoFavButton from "components/PhotoFavButton";
-import { on } from "events";
+// import { on } from "events";
 
 const PhotoDetailsModal = (props) => {
   const {
     photos,
     favoritedPhotos,
     activePhoto,
+    onPhotoSelect,
     updateToFavPhotoIds,
     onClosePhotoDetailsModal,
-    // modalVisibility,
-    toggleFavourite,
   } = props;
 
   const handleCloseModal = () => {
     onClosePhotoDetailsModal();
-  }
+  };
 
-  // Adding a null check for activePhoto
-  if (!activePhoto || !modalVisibility) {
-    return null;
-  }
+  // // Adding a null check for activePhoto
+  // if (!activePhoto || !modalVisibility) {
+  //   return null;
+  // }
 
-  // Logging activePhoto to the console
-  console.log(activePhoto);
+  // // Logging activePhoto to the console
+  // console.log(activePhoto);
 
-  
+  const similarPhotoArray = Object.values(activePhoto.similar_photos);
+  const similarPhotoIDs = similarPhotoArray.map((photo) => photo.id);
+  const similarPhotos = photos.filter((photo) =>
+    similarPhotoIDs.includes(photo.id)
+  );
+
 
   return (
     <div className="photo-details-modal">
       <button
-        className="photo-details-modal__close-button"
-        onClick={() => setModalVisibility(false)}
-      >
-        <img src={closeSymbol} alt="close symbol" />
+        className="photo-details-modal__close-button">
+        <img src={closeSymbol} alt="Close" onClick={handleCloseModal} />
       </button>
 
       <div className="photo-details-modal__images">
         <PhotoFavButton
           updateToFavPhotoIds={updateToFavPhotoIds}
           photo={activePhoto}
-          favorites={favoritedPhotos}
+          favoritedPhotos={favoritedPhotos}
         />
 
         <img
@@ -58,7 +60,7 @@ const PhotoDetailsModal = (props) => {
 
         <div className="photo-details-modal__photographer-details">
           <img
-            src={activePhoto.user.profile_image.small}
+            src={activePhoto.user.profile}
             alt={activePhoto.user.name}
             className="photo-details-modal__photographer-profile"
           />
@@ -72,23 +74,22 @@ const PhotoDetailsModal = (props) => {
             </div>
           </div>
         </div>
-
-        <div className="photo-details-modal__header">
-          Similar Photos
-        </div>
+      </div>
 
         <div className="photo-details-modal__images">
-          <PhotoList
-            photos={activePhoto.similar_photos}
-            favoritedPhotos={favoritedPhotos} 
-            updateToFavPhotoIds={updateToFavPhotoIds}
-            onPhotoSelect={onPhotoSelect}
-          />
-        </div>
-
-      </div>
+          <h2 className="photo-details-modal__header">Similar Photos</h2>
+          </div>
+          {activePhoto && activePhoto.similar_photos && (
+            <PhotoList
+              photos={similarPhotos}
+              favoritedPhotos={favoritedPhotos}
+              updateToFavPhotoIds={updateToFavPhotoIds}
+              onPhotoSelect={onPhotoSelect}
+            />  
+          )}
     </div>
   );
-};
+}
+
 
 export default PhotoDetailsModal;
