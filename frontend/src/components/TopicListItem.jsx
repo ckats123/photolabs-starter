57b1cd2b529b045e94b2dbe0db1ef961453edb18
Sophia <chiaki.katsuno@live.ca>
2024-02-27 22:18:34 -0800

    cleanup file

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -2,16 +2,17 @@ import React from "react";
 
 import "../styles/TopicListItem.scss";
 
-
 const TopicListItem = (props) => {
   const { data, onLoadTopic } = props;
   const handleTopicClick = () => {
     onLoadTopic(data);
   };
 
-  return <div className="topic-list__item">
-    <span onClick={handleTopicClick}>{data.title}</span>
-  </div>;
+  return (
+    <div className="topic-list__item">
+      <span onClick={handleTopicClick}>{data.title}</span>
+    </div>
+  );
 };
 
 export default TopicListItem;
