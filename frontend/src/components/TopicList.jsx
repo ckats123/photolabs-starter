5b0f9152cabeec19562e2970b50184b7f8134a3e
Sophia <chiaki.katsuno@live.ca>
2024-02-27 21:24:13 -0800

    render topics

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -5,12 +5,18 @@ import TopicListItem from "./TopicListItem";
 
 const TopicList = (props) => {
   const topics = props.topics.map((topic) => {
-    return 
-    <TopicListItem key={topic.id} data={topic} onLoadTopic={props.onLoadTopic} />;
-  }
-  );
-  return 
-  <div className="top-nav-bar__topic-list">{topics}</div>;
+    return (
+      <TopicListItem
+        key={topic.id}
+        data={topic}
+        onLoadTopic={props.onLoadTopic}
+      />
+    );
+  });
+  return <div className="top-nav-bar__topic-list">{topics}</div>;
 };
+
+
+
 
 export default TopicList;
